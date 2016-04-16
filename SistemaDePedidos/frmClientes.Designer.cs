ede68eb42e229f9944f5d26eccfbdd34d552490f
lucasmarqs <lucas.smarques@outlook.com>
2016-04-15 21:02:32 -0300

    feat: implementa tela de clientes e método de navegação

--- SistemaDePedidos/frmClientes.Designer.cs
+++ SistemaDePedidos/frmClientes.Designer.cs
@@ -28,19 +28,314 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tCode = new System.Windows.Forms.TextBox();
+            this.tCompanyName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tCnpj = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tCity = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tZipcode = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cmbState = new System.Windows.Forms.ComboBox();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnAlter = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbState);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.tZipcode);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.tCity);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.tAddress);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.tCnpj);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tCompanyName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tCode);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(606, 166);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cadastro";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Controls.Add(this.btnOk);
+            this.panel1.Controls.Add(this.btnAlter);
+            this.panel1.Controls.Add(this.btnDel);
+            this.panel1.Controls.Add(this.btnNew);
+            this.panel1.Controls.Add(this.btnLast);
+            this.panel1.Controls.Add(this.btnNext);
+            this.panel1.Controls.Add(this.btnPrev);
+            this.panel1.Controls.Add(this.btnFirst);
+            this.panel1.Location = new System.Drawing.Point(-2, 184);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(633, 34);
+            this.panel1.TabIndex = 1;
+            // 
+            // tCode
+            // 
+            this.tCode.Location = new System.Drawing.Point(9, 43);
+            this.tCode.Name = "tCode";
+            this.tCode.Size = new System.Drawing.Size(100, 20);
+            this.tCode.TabIndex = 1;
+            // 
+            // tCompanyName
+            // 
+            this.tCompanyName.Location = new System.Drawing.Point(115, 43);
+            this.tCompanyName.Name = "tCompanyName";
+            this.tCompanyName.Size = new System.Drawing.Size(347, 20);
+            this.tCompanyName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Razão Social";
+            // 
+            // tCnpj
+            // 
+            this.tCnpj.Location = new System.Drawing.Point(468, 43);
+            this.tCnpj.Name = "tCnpj";
+            this.tCnpj.Size = new System.Drawing.Size(130, 20);
+            this.tCnpj.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(465, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "CNPJ";
+            // 
+            // tAddress
+            // 
+            this.tAddress.Location = new System.Drawing.Point(9, 86);
+            this.tAddress.Name = "tAddress";
+            this.tAddress.Size = new System.Drawing.Size(299, 20);
+            this.tAddress.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Endereço";
+            // 
+            // tCity
+            // 
+            this.tCity.Location = new System.Drawing.Point(314, 86);
+            this.tCity.Name = "tCity";
+            this.tCity.Size = new System.Drawing.Size(221, 20);
+            this.tCity.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(311, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Cidade";
+            // 
+            // tZipcode
+            // 
+            this.tZipcode.Location = new System.Drawing.Point(9, 132);
+            this.tZipcode.Name = "tZipcode";
+            this.tZipcode.Size = new System.Drawing.Size(100, 20);
+            this.tZipcode.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 116);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "CEP";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(541, 70);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(21, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "UF";
+            // 
+            // cmbState
+            // 
+            this.cmbState.FormattingEnabled = true;
+            this.cmbState.Location = new System.Drawing.Point(541, 86);
+            this.cmbState.Name = "cmbState";
+            this.cmbState.Size = new System.Drawing.Size(57, 21);
+            this.cmbState.TabIndex = 13;
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(22, 3);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(37, 23);
+            this.btnFirst.TabIndex = 2;
+            this.btnFirst.Text = "<<";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(65, 3);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(37, 23);
+            this.btnPrev.TabIndex = 3;
+            this.btnPrev.Text = "<";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(108, 3);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(37, 23);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(151, 3);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(37, 23);
+            this.btnLast.TabIndex = 5;
+            this.btnLast.Text = ">>";
+            this.btnLast.UseVisualStyleBackColor = true;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(249, 3);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(56, 23);
+            this.btnNew.TabIndex = 6;
+            this.btnNew.Text = "Novo";
+            this.btnNew.UseVisualStyleBackColor = true;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(311, 3);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(56, 23);
+            this.btnDel.TabIndex = 3;
+            this.btnDel.Text = "Excluir";
+            this.btnDel.UseVisualStyleBackColor = true;
+            // 
+            // btnAlter
+            // 
+            this.btnAlter.Location = new System.Drawing.Point(373, 3);
+            this.btnAlter.Name = "btnAlter";
+            this.btnAlter.Size = new System.Drawing.Size(56, 23);
+            this.btnAlter.TabIndex = 7;
+            this.btnAlter.Text = "Alterar";
+            this.btnAlter.UseVisualStyleBackColor = true;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(508, 3);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(37, 23);
+            this.btnOk.TabIndex = 3;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(551, 3);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(60, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = true;
             // 
             // frmClientes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(662, 318);
+            this.ClientSize = new System.Drawing.Size(626, 215);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "frmClientes";
             this.Text = "Módulo de Clientes";
+            this.Load += new System.EventHandler(this.frmClientes_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbState;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tZipcode;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tCity;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tCnpj;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tCompanyName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tCode;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnAlter;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnFirst;
     }
 }
--- SistemaDePedidos/frmClientes.cs
+++ SistemaDePedidos/frmClientes.cs
@@ -12,9 +12,51 @@ namespace SistemaDePedidos
 {
     public partial class frmClientes : Form
     {
+        enum OperationMode
+        {
+            Navigation,
+            Edition
+        }
+
         public frmClientes()
         {
             InitializeComponent();
+        }
+
+        private void operationMode(OperationMode mode)
+        {
+            switch (mode)
+            {
+                case OperationMode.Navigation:
+                    btnOk.Enabled = false;
+                    btnCancel.Enabled = false;
+                    btnFirst.Enabled = true;
+                    btnPrev.Enabled = true;
+                    btnNext.Enabled = true;
+                    btnLast.Enabled = true;
+                    btnNew.Enabled = true;
+                    btnDel.Enabled = true;
+                    btnAlter.Enabled = true;
+                    break;
+
+                case OperationMode.Edition:
+                    btnOk.Enabled = true;
+                    btnCancel.Enabled = true;
+                    btnFirst.Enabled = false;
+                    btnPrev.Enabled = false;
+                    btnNext.Enabled = false;
+                    btnLast.Enabled = false;
+                    btnNew.Enabled = false;
+                    btnDel.Enabled = false;
+                    btnAlter.Enabled = false;
+                    break;
+            }
+            
+        }
+
+        private void frmClientes_Load(object sender, EventArgs e)
+        {
+            operationMode(OperationMode.Navigation);
         }
     }
 }
